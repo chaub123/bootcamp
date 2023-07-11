8b7fc6357e42f5050727a39d89cec397763365e9
chaub123 <kinchau@hotmail.com.hk>
2023-07-11 16:54:30 +0800

    modified:   day2/JavaQuest1.java 	new file:   demo-helloworld/src/JavaQuest1.java

--- demo-helloworld/src/JavaQuest1.java
+++ demo-helloworld/src/JavaQuest1.java
@@ -21,35 +21,30 @@ public class JavaQuest1 {
 
     System.out.println("I am going to add two numbers together:");
     int first = 31;
-    // declare the second one
     int second = 42;
+    // declare the second one
     // code here
 
     System.out.println("The first one: " + first);
     System.out.println("The second one: " + second);
 
     // Add two numbers and print it out
-    int sum = first + second;
     // code here
     System.out.println("The sum is " + sum);
 
     // subtraction
     // code here with tips, pls fix
     // subtract = first - second
-    int subtract = second - first;
     System.out.println("The subtraction reuslt is " + subtract);
 
     // another sum
     byte b1 = 60;
     byte b2 = 70;
     // code here with tips, pls fix
-    sum = b1 + b2;
     // sum = b1 + b2;
     System.out.println("The another sum reuslt is " + sum);
 
     // declare two boolean variable with true and false value
-    boolean varB = false;
-    boolean varC = true;
     // code here
     System.out.println("The variable varB is " + varB);
     System.out.println("The variable varC is " + varC);
@@ -57,7 +52,6 @@ public class JavaQuest1 {
     // Reading Example
     float fnumber = -130.20f; // By default, numbers are treated as double in Java.
     // Use suffix 'f' with value to make it float.
-    float dnumber = -20.5f;
     // code here 
     System.out.println("The variable fnumber is " + fnumber);
     System.out.println("The variable dnumber is " + dnumber);
@@ -66,16 +60,15 @@ public class JavaQuest1 {
     int a = 7;
     int b = 3;
     // Fix it
-    int reminder = a % 3;
+    int reminder = a - b;
     System.out.println("The variable reminder is " + reminder);
 
     // declare a variable x and assign value to it.
     int e = 7;
     int o = 8;
     int y = 4;
-    int x = 2;
     // code here
-    int result = e - o * x / y;  
-    System.out.println("The variable result is " + result);
+    int result = e - o * x / y;
+    System.out.println("The result is " + result);
   }
 }
